a198fac825ebc56ad9e87ec35725e77fd35cabfc
JenifferPatrick <jenifferpatrick6@gmail.com>
2025-02-22 23:59:01 +0300

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
 // Function to generate and print Fibonacci Series up to n terms
 void printFibonacci(int n) {
     int first = 0, second = 1, next;
 
     cout << "Fibonacci Series up to " << n << " terms:\n";
-    
+
     for (int i = 1; i <= n; i++) {
         cout << first << " ";  // Print the current Fibonacci number
         next = first + second;  // Compute the next term
@@ -24,8 +26,12 @@ int main() {
     cout << "Enter the number of Fibonacci terms: ";
     cin >> fibTerms;
 
-    // Call Fibonacci function
+    // Measure runtime
+    auto start = high_resolution_clock::now();
     printFibonacci(fibTerms);
+    auto stop = high_resolution_clock::now();
+
+    cout << "Time taken: " << duration_cast<microseconds>(stop - start).count() << " microseconds\n";
 
     return 0;
 }
